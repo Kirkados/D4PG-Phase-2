92717813f8a078dcd797cf30be07302d8bab8d75
Kirkados <khovell@gmail.com>
2020-04-14 15:12:06 -0400

    fixed the angle unwrapping and modified down = up

--- use_deep_guidance.py
+++ use_deep_guidance.py
@@ -364,6 +364,8 @@ def main():
             sleep(0.1)
             g.set_guided_mode()
             sleep(0.2)
+            last_target_yaw = 0.0
+            last_chaser_yaw = 0.0
             while True:
                 # TODO: make better frequency managing
                 sleep(g.step)
@@ -377,13 +379,16 @@ def main():
                         policy_input[4] =  rc.X[0] # target X [north] =   North
                         policy_input[5] = -rc.X[1] # targey Y [west]  = - East
                         policy_input[6] =  rc.X[2] # target Z [up]    =   Up
-                        policy_input[7] =  np.unwrap(rc.W[2]) # target yaw 
+                        policy_input[7] =  np.unwrap([last_target_yaw, rc.W[2]])[1] # target yaw 
+                        last_target_yaw = policy_input[7]
                         # Note: rc.X returns position; rc.V returns velocity; rc.W returns attitude
                     if rc.id == follower_id: # we've found the chaser (follower)
                         policy_input[0] =  rc.X[0] # chaser X [north] =   North
                         policy_input[1] = -rc.X[1] # chaser Y [west]  = - East
                         policy_input[2] =  rc.X[2] # chaser Z [up]    =   Up
-                        policy_input[3] =  np.unwrap(rc.W[2]) # chaser yaw
+                        
+                        policy_input[3] =  np.unwrap([last_chaser_yaw, rc.W[2]])[1] # chaser yaw
+                        last_chaser_yaw = policy_input[3]
                         # Note: rc.X returns position; rc.V returns velocity; rc.W returns attitude
                     
                 

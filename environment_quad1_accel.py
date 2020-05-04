320853ab2ce42bfdef00244b5d5b9a9f304feb8e
Kirkados <khovell@gmail.com>
2020-05-04 13:26:53 -0400

    sweeping dynamics delay and action log

--- environment_quad1_accel.py
+++ environment_quad1_accel.py
@@ -91,7 +91,7 @@ class Environment:
         self.N_STEP_RETURN            =   1
         self.DISCOUNT_FACTOR          =   0.95**(1/self.N_STEP_RETURN)
         self.TIMESTEP                 =   0.2 # [s]
-        self.DYNAMICS_DELAY           =   4 # [timesteps of delay] how many timesteps between when an action is commanded and when it is realized
+        self.DYNAMICS_DELAY           =   3 # [timesteps of delay] how many timesteps between when an action is commanded and when it is realized
         self.STATE_AUGMENT_LENGTH     =   5 # [timesteps] how many timesteps of previous actions should be included in the state. This helps with making good decisions among delayed dynamics.
         self.TARGET_REWARD            =   1. # reward per second
         self.FALL_OFF_TABLE_PENALTY   =   0.
--- settings.py
+++ settings.py
@@ -16,8 +16,8 @@ class Settings:
     ##### Run Settings #####
     ########################
 
-    RUN_NAME               = 'velocity_test' # use just the name. If trying to restore from file, use name along with timestamp
-    ENVIRONMENT            = 'quad1'
+    RUN_NAME               = 'delay_3_log_5' # use just the name. If trying to restore from file, use name along with timestamp
+    ENVIRONMENT            = 'quad1_accel'
     RECORD_VIDEO           = True
     VIDEO_RECORD_FREQUENCY = 20 # Multiples of "CHECK_GREEDY_PERFORMANCE_EVERY_NUM_EPISODES"
     NOISELESS_AT_TEST_TIME = True # Whether or not to test without action noise (Keep at True unless debugging)

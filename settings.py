ba5a3f11629d2d8a683726bb397c8838d14e6dd0
Kirkados <khovell@gmail.com>
2020-10-10 11:23:38 -0400

    office sim2

--- settings.py
+++ settings.py
@@ -16,8 +16,8 @@ class Settings:
     ##### Run Settings #####
     ########################
 
-    RUN_NAME               = 'accel_10Hz_delay3' # use just the name. If trying to restore from file, use name along with timestamp
-    ENVIRONMENT            = 'quad1_accel' # 'quad1' for Task 1 velocity; 'quad1_accel' for Task 1 accel; 'quad1_runway' for Task 2 accel
+    RUN_NAME               = 'vel_5Hz_delay3' # use just the name. If trying to restore from file, use name along with timestamp
+    ENVIRONMENT            = 'quad1' # 'quad1' for Task 1 velocity; 'quad1_accel' for Task 1 accel; 'quad1_runway' for Task 2 accel
     AGENT                  = '' # '' for Task 1, '_runway' for runway experiment
     RECORD_VIDEO           = True
     VIDEO_RECORD_FREQUENCY = 20 # 20 Multiples of "CHECK_GREEDY_PERFORMANCE_EVERY_NUM_EPISODES"

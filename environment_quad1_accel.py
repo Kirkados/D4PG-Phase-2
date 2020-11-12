889095ffa54fa705f1975a56ccf3914aa7cb383a
Kirkados <khovell@gmail.com>
2020-11-12 10:42:51 -0500

    another trial

--- environment_quad1_accel.py
+++ environment_quad1_accel.py
@@ -116,8 +116,8 @@ class Environment:
         self.N_STEP_RETURN                    =   2 ####*********%%%%%% SET TO 1 -> Used to be set to 5 ^^$%*$%*#$$&@&#*$*
         self.DISCOUNT_FACTOR                  =   0.95**(1/self.N_STEP_RETURN)
         self.TIMESTEP                         =   0.2 # [s]
-        self.DYNAMICS_DELAY                   =   5 # [timesteps of delay] how many timesteps between when an action is commanded and when it is realized
-        self.AUGMENT_STATE_WITH_ACTION_LENGTH =   5 # [timesteps] how many timesteps of previous actions should be included in the state. This helps with making good decisions among delayed dynamics.
+        self.DYNAMICS_DELAY                   =   3 # [timesteps of delay] how many timesteps between when an action is commanded and when it is realized
+        self.AUGMENT_STATE_WITH_ACTION_LENGTH =   3 # [timesteps] how many timesteps of previous actions should be included in the state. This helps with making good decisions among delayed dynamics.
         self.TARGET_REWARD                    =   1. # reward per second
         self.FALL_OFF_TABLE_PENALTY           =   0.
         self.END_ON_FALL                      = False # end episode on a fall off the table
